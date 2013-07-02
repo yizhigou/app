53d5fa79751d257af5835f96cbe0d8124f1fe05c
Wei Tang <twtwwei@gmail.com>
2013-07-02 12:55:13 +0800

    Text modify
    Modify text discribe

--- wp-includes/comment-template.php
+++ wp-includes/comment-template.php
@@ -1354,7 +1354,7 @@ class Walker_Comment extends Walker {
 		<?php printf(__('<cite class="fn">%s</cite> <span class="says">says:</span>'), get_comment_author_link()) ?>
 		</div>
 <?php if ($comment->comment_approved == '0') : ?>
-		<em class="comment-awaiting-moderation"><?php _e('Your comment is awaiting moderation.') ?></em>
+		<em class="comment-awaiting-moderation"><?php _e('评论正在审核中.') ?></em>
 		<br />
 <?php endif; ?>
 
